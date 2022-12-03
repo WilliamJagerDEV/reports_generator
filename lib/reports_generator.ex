659036669e224f0afa1991cf0d796ee3361e1003
WilliamJagerDEV <williamjager.dev@gmail.com>
2022-12-03 15:42:47 -0300

    sum report values

--- lib/reports_generator.ex
+++ lib/reports_generator.ex
@@ -10,13 +10,22 @@ defmodule ReportsGenerator do
 
   def build(filename) do
     "reports/#{filename}"
-    |> File.read()
-    |> handle_build()
+    # retorna metadados e path do arquivo
+    |> File.stream!()
+    |> Enum.reduce(report_acc(), fn line, report ->
+      [id, _food_name, price] = parse_line(line)
+      Map.put(report, id, report[id] + price)
+    end)
   end
 
-  defp handle_build({:ok, result}) do
-    result
+  defp parse_line(line) do
+    line
+    |> String.trim()
+    |> String.split(",")
+    # atualiza terceira posição da lista substituindo string por inteiro
+    |> List.update_at(2, &String.to_integer/1)
   end
 
-  defp handle_build({:error, _reason}), do: {:error, "Invalid file name."}
+  # cria um map com chaves de 1 a 30 para o .csv completo
+  defp report_acc, do: Enum.into(1..30, %{}, &{Integer.to_string(&1), 0})
 end
